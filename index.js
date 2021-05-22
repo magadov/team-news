ff3be3721b8e88437b09d2399df7558014795349
Vis <awwya@mail.ru>
2021-05-22 21:29:39 +0300

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
-
+const data = require('./data.js')
 const cors = require('cors');
-const hbc = require('express-handlebars');
+const hbs = require('express-handlebars');
+
 
 app.use(cors());
 app.set('view engine', 'handlebars');
-app.engine('handlebars', hbc());
+app.engine('handlebars', hbs());
+
+
+
+app.get('/', (req, res) => {
+  res.render("home")
+})
+
+app.get('/politic/:id', (req, res) => {
+  res.render("politic")
+})
+
+app.get('/sport/:id', (req, res) => {
+  res.render("home")
+})
+
+app.get('/IT/:id', (req, res) => {
+  res.render("IT")
+})
+
+app.get('/contacts', (req, res) => {
+  res.render("home")
+})
 
 app.listen(3030, () => {
   console.log('Server has been started...')
